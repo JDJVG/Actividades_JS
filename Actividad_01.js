c17a60330e987c612c32722b0d3a1669d098bf82
Jesus <g123vj19@gmail.com>
2022-03-15 18:15:43 -0600

    Add Actividad_01

---
+++ Actividad_01.js
@@ -0,0 +1,20 @@
+// Actividad_01
+
+let numero1 = Number(prompt("Numero"))
+let operacion = numero1 * 2
+console.log(operacion)
+    
+let dato1= Number(prompt("Introduce un numero"))
+let dato2 = Number(prompt("Introduce un numero"))
+let operacion = dato1 - dato2
+console.log(operacion)    
+
+var radio= 5.3
+var pi = 3.1416
+var area = radio * pi ** 2
+console.log(area)
+
+var D = 8
+var d = 6
+var area = D * d / 2
+console.log(area)
